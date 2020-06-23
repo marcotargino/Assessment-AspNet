01d14f1c3c75c126fde5b43c0c590ca5a1b47927
marcotargino <64049052+marcotargino@users.noreply.github.com>
2020-06-23 14:45:11 -0300

    final version
    at completo

--- BirthdayManager/Models/Person.cs
+++ BirthdayManager/Models/Person.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +8,8 @@ namespace BirthdayManager.Models
     public class Person
     {
         public int Id { get; set; }
-        public String Firstname { get; set; }
-        public String Lastname { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
         public DateTime Birthdate { get; set; }
         public int Age { get; set; }
     }
